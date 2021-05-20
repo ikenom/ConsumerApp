c8c8d166d6ff722082398643451c609efa5b7312
Robert Leonard <rleonard@ojolabs.com>
2021-05-19 22:48:45 -0400

    Adding text atom

--- stories/index.js
+++ stories/index.js
@@ -2,3 +2,4 @@ import './Button/Button.stories';
 import './Welcome/Welcome.stories';
 import './atoms/layout/Box.stories';
 import './atoms/layout/Flexbox.stories';
+import './atoms/typography/Typography.stories';
