c118818150932b1547ecc8d87d9a8f185ce64b12
Robert Leonard <rleonard@ojolabs.com>
2021-05-22 18:36:29 -0400

    Adding react-native vector icons

---
+++ src/defaultTheme.ts
@@ -0,0 +1,42 @@
+export const defaultTheme = {
+  colors: {
+    black: "#121212",
+    white: "#FFFFFF",
+  },
+  fontFamily: {
+    dual_sm: "Dual-300",
+    dual_lg: "Dual-600",
+    hnt: "Helvetica Now Text",
+    hnt_light: "Helvetica Now Text Light",
+    hnt_medium: "Helvetica Now Text Medium",
+    hnt_bold: "Helvetica Now Text Bold",
+    hnt_extra_bold: "Helvetica Now Text Extra Bold",
+    hnt_black: "Helvetica Now Text Black"
+  },
+  fontSize: {
+    xsm: "12px",
+    sm: "14px",
+    m: "16px",
+    lg: "28px",
+    xlg: "32px",
+    max: "42px",
+  },
+  lineHeight: {
+    default: "16.14",
+  },
+  radii: {
+    xs: "4px",
+    sm: "8px",
+    md: "12px",
+    lg: "20px",
+    circle: "50%",
+    pill: "500px"
+  },
+  breakpoints: {
+    xs: '300px',
+    sm: '600px',
+    md: '960px',
+    lg: '1280px',
+    xl: '1920px',
+  },
+};
