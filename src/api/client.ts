514bcc18b23edeb1ce82f025575f983f9084673d
Robert Leonard <rleonard@ojolabs.com>
2021-07-06 15:08:46 -0400

    final additions to auth store

--- src/api/client.ts
+++ src/api/client.ts
@@ -9,14 +9,12 @@ import {
   gql
 } from '@apollo/client';
 import { onError } from "@apollo/client/link/error";
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import 'cross-fetch/polyfill';
 import ActionCableLink from 'graphql-ruby-client/subscriptions/ActionCableLink';
-import { Alert } from 'react-native';
+import AuthStore from '../store/authStore';
 
 
-const url = "http://127.0.0.1:64973"
-const testGraphql = "https://graphql.org/swapi-graphql"
+const url = "http://127.0.0.1:54678"
+
 
 let cable;
 if (typeof window !== 'undefined') {
@@ -27,11 +25,6 @@ if (typeof window !== 'undefined') {
 const httpLink = createHttpLink({
   uri: `${url}/graphql`
 });
-
-const testHttpLink = createHttpLink({
-  uri: testGraphql
-});
-
 
 const errorLink = onError(({ graphQLErrors, networkError }) => {
   if (graphQLErrors)
@@ -56,19 +49,6 @@ const link = ApolloLink.split(
   from([errorLink, httpLink])
 );
 
-
-const authMiddleware = new ApolloLink((operation, forward) => {
-  const token = undefined
-  
-  operation.setContext({
-    headers: {
-      authorization: token ? `Bearer ${token}` : ''
-    }
-  });
-
-  return forward(operation);
-})
-
 const defaultOptions: DefaultOptions = {
   watchQuery: {
     fetchPolicy: 'no-cache',
@@ -77,57 +57,31 @@ const defaultOptions: DefaultOptions = {
   query: {
     fetchPolicy: 'no-cache',
     errorPolicy: 'all',
-  },
-  mutate: {
-    fetchPolicy: 'no-cache',
-    errorPolicy: 'all',
-    awaitRefetchQueries: true,
   }
 }
+
+
+const getToken = async () => {
+  const t = await AuthStore.getToken()
+  return t
+}
+
+const authMiddleware = new ApolloLink((operation, forward) => {  
+  getToken().then(token => {
+    operation.setContext({
+      headers: {
+        authorization: token ? `Bearer ${token}` : '',
+      },
+    })
+  })
+
+  return forward(operation);
+})
+
 
 export const client = new ApolloClient({
   cache: new InMemoryCache(),
   link: concat(authMiddleware, link),
   defaultOptions: defaultOptions
 });
-
-const TEST = gql`
-query {
-  allFilms {
-    films {
-      title
-    }
-  }
-}
-`
-
-export const testClient = new ApolloClient({
-  cache: new InMemoryCache(),
-  link: concat(authMiddleware, testHttpLink),
-  defaultOptions: defaultOptions
-});
-
-export const callTest = async () => {
-  console.log("bleh")
-  const result = await testClient.query({
-    query: TEST
-  })
-  console.log(`Result: ${result}`)
-}
-
-export const getMoviesFromApiAsync = async () => {
-  try {
-    console.log("bleh getting test data")
-    let response = await fetch(
-      'https://reactnative.dev/movies.json'
-    );
-    console.log("Got test data")
-    let json = await response.json();
-    Alert.alert(`Resolved data is: ${json}`)
-    return json.movies;
-  } catch (error) {
-    console.error(error);
-  }
-};
-
 
--- src/store/authStore.ts
+++ src/store/authStore.ts
@@ -1,14 +1,13 @@
 import * as authClient from '../api/auth_client'
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { Alert } from 'react-native';
-
 export default class AuthStore {
 
   private static instance: AuthStore;
 
+
   static init = async () => {
     const authStore = AuthStore.getInstance();
-    console.log("Starting call to backend")
     await authStore.loginAsync("bong.oconnell@spencer.org", "password")
   }
 
@@ -22,21 +21,15 @@ export default class AuthStore {
 
   loginAsync = async (email: String, password: String) => {
     const data = await authClient.login(email, password)
-    this.saveToken(data.login.token)
-    await authClient.test()
-    Alert.alert("Done")
+    await this.saveToken(data.login.token)
+    console.log("Successfully logged in")
   }
 
-  saveToken = (token: string) => {
-    AsyncStorage.setItem("token", token);
+  saveToken = async (token: string) => {
+    await AsyncStorage.setItem("token", token);
   }
 
-  getToken = async (): Promise<String | null> => {
+  static getToken = async (): Promise<String | null> => {
     return AsyncStorage.getItem("token")
   }
-
-}
-
-export class MockUserStore extends AuthStore{
-  init = async() => {}
 }
