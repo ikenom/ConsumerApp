8eac775c9611dcdaf454133ee808c5338d98c509
alwkins <75457552+alwkins@users.noreply.github.com>
2021-07-08 20:17:10 -0500

    This belongs with previous commit.

--- src/components/atoms/card/util.ts
+++ src/components/atoms/card/util.ts
@@ -8,13 +8,17 @@ export type MealCardType = 'horizontal' | 'vertical';
 export const VERTICAL_DIM: Dimension = {
   width: wp('44%'),
   height: hp('22%'),
-  contentHeight: hp('6.7%')
+  contentHeight: hp('6.7%'), 
+  truncateMealTo: 19,
+  truncateRestaurantTo: 19
 }
 
 export const HORIZONTAL_DIM: Dimension = {
   width: wp('58%'),
   height: hp('13%'),
-  contentHeight: hp('5%')
+  contentHeight: hp('5%'), 
+  truncateMealTo: 27,
+  truncateRestaurantTo: 25,
 }
 
 export const getMealCardLayoutDimensions = (layoutType: MealCardType): Dimension => {
@@ -25,4 +29,6 @@ export interface Dimension {
   width: number;
   height: number;
   contentHeight: number;
+  truncateMealTo: number;
+  truncateRestaurantTo: number;
 } 
