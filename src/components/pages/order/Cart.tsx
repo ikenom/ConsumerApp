4abcdd5ca3360cb8c0b7bbf6d9eab31248aa8f7e
Robert Leonard <rleonard@ojolabs.com>
2021-07-10 10:31:57 -0400

    Allowing user to checkout with ordre

--- src/components/pages/order/Cart.tsx
+++ src/components/pages/order/Cart.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import styled from "styled-components";
 import { Image, LogBox } from "react-native";
 import { Divider, Input } from 'react-native-elements';
@@ -6,7 +6,8 @@ import {
   widthPercentageToDP as wp,
   heightPercentageToDP as getHeightInPixels,
 } from 'react-native-responsive-screen';
-import { Button } from "react-native-elements/dist/buttons/Button";
+import { Button as IconButton} from "react-native-elements/dist/buttons/Button";
+import { Button } from "react-native-elements";
 import { defaultTheme } from "../../../defaultTheme";
 import { Box, FlexBox, FlexItem } from "../../atoms/layout/Box";
 import { Text } from "../../atoms/typography/Text";
@@ -19,8 +20,9 @@ import { TouchableOpacity } from "react-native-gesture-handler";
 import { OrderConfirmationHeader } from "./common/OrderConfirmationHeader";
 import { StackNavigationProp, StackScreenProps } from "@react-navigation/stack";
 import { RestaurantParamList } from "../../../../App";
-import { createMockOrderWithItems } from "../../../models/order/util";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
+import OrderStore from "../../../store/orderStore";
+import { useState } from "@hookstate/core";
 
 LogBox.ignoreLogs(['margin', 'width']);
 
@@ -53,13 +55,16 @@ type Fulfillment = typeof fulfillment[number]
 export const OrderConfirmationCart = (props: OrderConfirmationCartProps) => {
 
   const insets = useSafeAreaInsets();
+
+  const orderStore = OrderStore.getInstance();
+  const isCheckingOut = useState(orderStore.isCheckingOut)
   
   const { restaurant, meal, navigation } = props;
   const fees = (Number.parseFloat(meal.price) * .07);
   const total = fees + Number.parseFloat(meal.price)
 
-  const [fulfillmentType, setFulfillmentType] = useState<Fulfillment>("Pickup");
-  const [additionalNotes, setAdditionalNotes] = useState("");
+  const [fulfillmentType, setFulfillmentType] = React.useState<Fulfillment>("Pickup");
+  const [additionalNotes, setAdditionalNotes] = React.useState("");
 
   const onPressFulfillment = (selection: Fulfillment) => {
     if(selection !== "Delivery") {
@@ -71,10 +76,14 @@ export const OrderConfirmationCart = (props: OrderConfirmationCartProps) => {
     navigation?.pop();
   }
 
-  const onConfirmOrder = () => {
-    const mockOrder = createMockOrderWithItems([meal]);
-    console.log(mockOrder)
-    navigation?.push('ConfirmationView', {restaurant, order: mockOrder})
+  const onConfirmOrder = async () => {
+
+    const callback = () => {
+      const order = orderStore.order.get()
+      navigation?.push('ConfirmationView', {restaurant, order})
+    }
+    
+    await orderStore.checkout(callback)
   }
 
   return(
@@ -138,6 +147,10 @@ export const OrderConfirmationCart = (props: OrderConfirmationCartProps) => {
           iconRight={true}
           icon={<Text fontWeight={'700'} fontSize={'18px'} color={'#FFFFFF'}>{`$${meal.price}`}</Text>}
           onPress={onConfirmOrder}
+          disabled={isCheckingOut.get()}
+          disabledStyle={{backgroundColor: defaultTheme.colors.blue}}
+          loading={isCheckingOut.get()}
+          loadingStyle={{marginLeft: wp('42%')}}
         />
       </FlexBox>
     </FlexBox>
@@ -197,7 +210,7 @@ const PaymentMethod = () => {
             </FlexBox>
           </FlexItem>
         </FlexBox>
-        <Button onPress={() => {}} icon={<MaterialCommunityIcon name={"chevron-right"} size={25} color={"#FFFFFF"}/>} />
+        <IconButton onPress={() => {}} icon={<MaterialCommunityIcon name={"chevron-right"} size={25} color={"#FFFFFF"}/>} />
       </FlexBox>
     </FlexBox>
   )
