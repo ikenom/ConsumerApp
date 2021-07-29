5da3cdea0e96d0957ba900934c00d9d2e6bb88d4
alwkins <75457552+alwkins@users.noreply.github.com>
2021-07-28 20:25:48 -0500

    Revert "Begin setting up navigation structure."
    This reverts commit 1e5e080eb9074df2d29b05d729dfc61f65d1d266.

--- App.tsx
+++ App.tsx
@@ -2,11 +2,12 @@ import React, { useEffect, useState } from 'react';
 import { SafeAreaView, StatusBar, View } from 'react-native';
 import { MealNavigatorContainer, MealOrderView, MealViewProps } from './src/components/pages/order/Meal';
 import { defaultTheme } from './src/defaultTheme';
-import { NavigationContainer, NavigatorScreenParams } from '@react-navigation/native'
+import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack';
 import { SafeAreaProvider, useSafeAreaInsets } from 'react-native-safe-area-context';
 import { Platform } from 'react-native';
 import { DefaultTheme } from '@react-navigation/native';
+//import StorybookUIRoot from './storybook'; This will break promises so they never resolve so only uncomment when testing
 import { RestaurantNavigatorContainer, RestaurantViewProps } from './src/components/pages/order/Restaurant';
 import { OrderConfirmationCartContainer, OrderConfirmationCartProps } from './src/components/pages/order/Cart';
 import { ConfirmationNavigatorContainer, OrderConfirmationProps } from './src/components/pages/order/Confirmation';
@@ -14,22 +15,16 @@ import AuthStore from './src/store/authStore';
 import RestaurantStore from './src/store/restaurantStore';
 import { Restaurant } from './src/models/restaurant/restaurant';
 import { FlexBox } from './src/components/atoms/layout/Box';
-import { LoadingView } from './src/components/pages/Loading';
 import { Spinner } from 'native-base'
 import OrderStore from './src/store/orderStore';
-
-type RootStackParamList = {
-  Loading: undefined;
-  HomeStack: undefined;
-  RestaurantStack: NavigatorScreenParams<RestaurantParamList>;
-}
-
 export type RestaurantParamList = {
   RestaurantView: RestaurantViewProps
   MealView: MealViewProps
   CartView: OrderConfirmationCartProps
   ConfirmationView: OrderConfirmationProps
 }
+
+const RestaurantStack = createStackNavigator<RestaurantParamList>();
 
 const platformVersion = Platform.Version;
 
@@ -46,9 +41,6 @@ const App = () => {
   const [isLoadingRestaurant, setLoadingRestaurant] = useState(true);
   const [restaurant, setRestaurant] = useState<Restaurant>();
 
-  const RootStack = createStackNavigator<RootStackParamList>();
-  const RestaurantStack = createStackNavigator<RestaurantParamList>();
-
   const startup = async () => {
     await AuthStore.init()
     await OrderStore.init()
@@ -64,37 +56,26 @@ const App = () => {
     startup()
   },[])
 
-  const RestaurantStackScreens = () => (
-    <RestaurantStack.Navigator initialRouteName={'RestaurantView'} screenOptions={{ headerShown: false }}>
-      <RestaurantStack.Screen name="RestaurantView" component={RestaurantNavigatorContainer} initialParams={{ restaurant, meals: { all: restaurant?.meals!!, recommendations: restaurant?.meals!! } }} />
-      <RestaurantStack.Screen name="MealView" component={MealNavigatorContainer} />
-      <RestaurantStack.Screen name="CartView" component={OrderConfirmationCartContainer} />
-      <RestaurantStack.Screen name='ConfirmationView' component={ConfirmationNavigatorContainer} />
-    </RestaurantStack.Navigator>
-  );
-
   return (
-    <View style={{
-      paddingTop: 0,
-      flex: 1,
-      paddingRight: insets.right,
-      paddingLeft: insets.left,
-      backgroundColor: defaultTheme.colors.black
-    }} >
-      <StatusBar barStyle={'light-content'} />
-      <NavigationContainer theme={MyTheme}>
-        <RootStack.Navigator screenOptions={{ headerShown: false }}>
-          {isLoadingRestaurant ? (
-            <RootStack.Screen name="Loading" component={LoadingView} />
-          ) : (
-            <RootStack.Screen
-              name="RestaurantStack"
-              component={RestaurantStackScreens}
-              options={{ headerShown: false }}
-            />
-          )}
-        </RootStack.Navigator>
-      </NavigationContainer>
+    <View style={{paddingTop: 0, flex: 1, paddingRight: insets.right ,paddingLeft: insets.left, backgroundColor: defaultTheme.colors.black}} >
+      <StatusBar barStyle={'light-content'}/>
+      {
+        isLoadingRestaurant &&
+        <FlexBox justifyContent={'center'} alignItems={'center'} height={'100%'}>
+          <Spinner color={defaultTheme.colors.greyOne}/>
+        </FlexBox>
+      }
+      {
+        !isLoadingRestaurant &&
+        <NavigationContainer theme={MyTheme}>
+          <RestaurantStack.Navigator initialRouteName={'RestaurantView'} screenOptions={{headerShown: false}}>
+            <RestaurantStack.Screen name="RestaurantView" component={RestaurantNavigatorContainer} initialParams={{restaurant, meals: {all: restaurant?.meals!!, recommendations: restaurant?.meals!!}}}/>
+            <RestaurantStack.Screen name="MealView" component={MealNavigatorContainer}/>
+            <RestaurantStack.Screen name="CartView" component={OrderConfirmationCartContainer}/>
+            <RestaurantStack.Screen name='ConfirmationView' component={ConfirmationNavigatorContainer}/>
+          </RestaurantStack.Navigator>
+        </NavigationContainer>
+      }
     </View>
   )
   
